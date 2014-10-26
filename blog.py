db3e26277367eda5d85478357b1180659d6fa288
se000ra <se000ra.gmail.com>
2014-10-26 11:41:49 +0600

    logout added

--- blog.py
+++ blog.py
@@ -14,6 +14,10 @@ from flask import Flask, render_template, request, session, flash, redirect, \
         url_for, g
 
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
+DEBUG = True
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -21,13 +25,28 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+                request.form['password'] != app.config['PASSWORD']:
+                    error = 'Ivalid Credentials. Please try again.'
+        else:
+            session['login_in'] = True
+            return redirect(url_for('main'))
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 @app.route('/main')
 def main():
     return render_template('main.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run()
